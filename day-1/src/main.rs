f5559efee17c298ef92b2a0b1e46be18ad0522c1
Tomas Birbe <tomas.birbe@gmail.com>
2022-12-02 10:32:10 -0300

    main: add day-1

---
+++ day-1/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("/--- Regalos! ---/");
+    println!("1 - Agarrar la pala como dev");
+    println!("2 - Que se termine el verano");
+    println!("3 - Aprender Rust sin esfuerzo");
+}
